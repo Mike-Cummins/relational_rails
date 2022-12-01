f9c0926a01ba0ca009403c347fc484448949ca3d
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-12-01 08:10:59 -0800

    Completes user story 2

--- spec/features/dealerships/show_spec.rb
+++ spec/features/dealerships/show_spec.rb
@@ -2,14 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Dealerships Show' do
   before :each do
-    @kendall = Dealership.create!(name: 'Kendall')
-    @lithia = Dealership.create!(name: 'Lithia')
-    @power = Dealership.create!(name: 'Power')
+    @kendall = Dealership.create!(name: 'Kendall', offers_financing: true, offers_shuttle: false, rating: 3)
+    @lithia = Dealership.create!(name: 'Lithia', offers_financing: false, offers_shuttle: true, rating: 4)
+    @power = Dealership.create!(name: 'Power', offers_financing: true, offers_shuttle: true, rating: 5)
   end
 
   it 'shows individual dealerships by ID number and all of their data' do
     visit "/dealerships/#{@kendall.id}"
-
+    save_and_open_page
     expect(page).to have_content(@kendall.name)
-    expect(page).to have_content(@kendall.name)
+    expect(page).to have_content(@kendall.id)
+    expect(page).to have_content(@kendall.offers_financing)
+    expect(page).to have_content(@kendall.offers_shuttle)
+    expect(page).to have_content(@kendall.rating)
+    expect(page).to_not have_content(@lithia.name)
+    expect(page).to_not have_content(@power.name)
   end
+end
