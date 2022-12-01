3c4c0ddbcea993f4386f2882588c186fd9eae4be
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-12-01 14:04:07 -0800

    Add sad path

--- spec/features/dealerships/vehicles/index_spec.rb
+++ spec/features/dealerships/vehicles/index_spec.rb
@@ -7,11 +7,13 @@ RSpec.describe 'Dealerships Vehicles Index' do
         kendall = Dealership.create!(name: 'Kendall', offers_financing: true, offers_shuttle: false, rating: 3)
         corolla = kendall.vehicles.create!(make: 'Toyota', model: 'Corolla', year: 2021, sale_pending: false)
         camry = kendall.vehicles.create!(make: 'Toyota', model: 'Camry', year: 2022, sale_pending: false)
+        lithia = Dealership.create!(name: 'Lithia', offers_financing: true, offers_shuttle: true, rating: 4)
+        wrangler = lithia.vehicles.create!(make: 'Jeep', model: 'Wrangler', year: 2018, sale_pending: true)
         visit "/dealerships/#{kendall.id}/vehicles"
-        save_and_open_page
 
         expect(page).to have_content(camry.model)
         expect(page).to have_content(corolla.model)
+        expect(page).to_not have_content(wrangler.model)
       end
     end
   end
