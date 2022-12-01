bb404c4a40b46f32cc305808509febb123472766
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-12-01 15:39:01 -0800

    Complete user story 7

--- spec/features/dealerships/show_spec.rb
+++ spec/features/dealerships/show_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe 'Dealerships Show' do
     @kendall = Dealership.create!(name: 'Kendall', offers_financing: true, offers_shuttle: false, rating: 3)
     @lithia = Dealership.create!(name: 'Lithia', offers_financing: false, offers_shuttle: true, rating: 4)
     @power = Dealership.create!(name: 'Power', offers_financing: true, offers_shuttle: true, rating: 5)
+    @corolla = @kendall.vehicles.create!(make: 'Toyota', model: 'Corolla', year: 2021, sale_pending: false)
+    @camry = @kendall.vehicles.create!(make: 'Toyota', model: 'Camry', year: 2022, sale_pending: false)
   end
 
   it 'shows individual dealerships by ID number and all of their data' do
@@ -17,5 +19,11 @@ RSpec.describe 'Dealerships Show' do
     expect(page).to have_content(@kendall.rating)
     expect(page).to_not have_content(@lithia.name)
     expect(page).to_not have_content(@power.name)
+  end
+
+  it 'displays the total number of vehicles in inventory' do
+    visit "/dealerships/#{@kendall.id}"
+
+    expect(page).to have_content("Total Inventory: 2")
   end
 end
