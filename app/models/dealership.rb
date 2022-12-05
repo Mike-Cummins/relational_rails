7dd1c0c6b1086840384700162bf34c2ced439de6
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-12-05 15:31:12 -0800

    Refactor vehicles count

--- app/models/dealership.rb
+++ app/models/dealership.rb
@@ -1,6 +1,10 @@
 class Dealership < ApplicationRecord
   has_many :vehicles
   def self.date_order
-    self.all.order(:created_at).reverse
+    self.all.order(created_at: :desc)
+  end
+
+  def vehicles_count
+    vehicles.count
   end
 end
