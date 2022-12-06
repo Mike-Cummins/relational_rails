a15e90abcaf996fec4e3ad967503bdc109f66479
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-12-06 13:26:02 -0800

    Start writing tests

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,6 @@
 class Vehicle < ApplicationRecord
   belongs_to :dealership
+  def sort_by_alpha
+    
+  end
 end
--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Vehicle do
   it {should belong_to :dealership}
+  describe '#sort_by_alpha' do
+    it 'sorts vehicles alphabetically by model' do
+      camry = Vehicle.new(make: "Toyota", model: 'Camry', year: 2000, sale_pending: true)
+      
+    end
+  end
 end
