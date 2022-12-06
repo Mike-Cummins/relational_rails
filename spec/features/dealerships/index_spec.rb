0a9b7f9bf57cf37e6d6800788b87b69dfc155707
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-12-06 14:56:44 -0800

    Complete user story 17

--- spec/features/dealerships/index_spec.rb
+++ spec/features/dealerships/index_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Dealerships Index' do
   before :each do
     @kendall = Dealership.create!(name: 'Kendall', offers_financing: true, offers_shuttle: false, rating: 3, created_at: Time.now - 2.days)
-    @lithia = Dealership.create!(name: 'Lithia', offers_financing: false, offers_shuttle: true, rating: 4)
+    @lithia = Dealership.create!(name: 'Lithia', offers_financing: false, offers_shuttle: true, rating: 4, created_at: Time.now)
     @power = Dealership.create!(name: 'Power', offers_financing: true, offers_shuttle: true, rating: 5, created_at: Time.now - 4.days)   
   end
 
@@ -17,8 +17,10 @@ RSpec.describe 'Dealerships Index' do
 
   it 'sorts dealerships by most recent created_at date' do
     visit "/dealerships"
+save_and_open_page
 
     expect(page).to have_content(@kendall.created_at)
+    expect(page).to have_content(@lithia.created_at)
     expect(page).to have_content(@power.created_at)
     expect(@lithia.name).to appear_before(@kendall.name)
     expect(@kendall.name).to appear_before(@power.name)
