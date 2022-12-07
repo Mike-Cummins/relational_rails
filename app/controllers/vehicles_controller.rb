1a7972f9d96e30f290fcdfb6593c57f5f5a7a578
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-12-06 16:26:44 -0800

    Complete user story 20

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -32,6 +32,13 @@ class VehiclesController < ApplicationController
     redirect_to "/vehicles/#{vehicle.id}"
   end
 
+  def destroy
+    vehicle = Vehicle.find(params[:id])
+    vehicle.destroy
+
+    redirect_to '/vehicles'
+  end
+
   private
   def vehicle_params
     params.permit(:make, :model, :year, :sale_pending)
--- spec/features/vehicles/delete_spec.rb
+++ spec/features/vehicles/delete_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe 'Vehicle Delete' do
-  it 'has a link to delete vehicle from its show page' do
-    kendall = Dealership.create!(name: 'Kendall', offers_financing: true, offers_shuttle: false, rating: 3)
-    corolla = kendall.vehicles.create!(make: 'Toyota', model: 'Corolla', year: 2021, sale_pending: false)
-    camry = kendall.vehicles.create!(make: 'Toyota', model: 'Camry', year: 2022, sale_pending: false)
-    visit "/vehicles/#{corolla.id}"
+RSpec.describe 'Vehicles Delete' do
+  describe 'as a user' do
+    describe 'when I visit /vehicles/:id' do
+      before :each do
+        @kendall = Dealership.create!(name: 'Kendall', offers_financing: true, offers_shuttle: false, rating: 3)
+        @corolla = @kendall.vehicles.create!(make: 'Toyota', model: 'Corolla', year: 2021, sale_pending: false)
+      end
+       
+      it 'has a link to delete the vehicle' do   
+        visit "/vehicles/#{@corolla.id}"
 
-    click_button("Delete Vehicle")
+        click_button("Delete Vehicle")
 
-    expect(current_path).to eq('/vehicles')
-    expect(page).to_not  have_content(corolla.model)
-    expect(page).to have_content(camry.model)
+        expect(current_path).to eq('/vehicles')
+        expect(page).to_not have_content(@corolla.model)
+      end
+    end
   end
 end
