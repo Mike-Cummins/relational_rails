04739a8f65ad6d794af47e0a77f13c718c341dbf
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-12-06 16:02:15 -0800

    Refactor

--- spec/features/dealerships/delete_spec.rb
+++ spec/features/dealerships/delete_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe 'Dealership Delete' do
     describe 'when I visit /dealerships/:id' do
       it 'has a link to delete that parent' do
         kendall = Dealership.create!(name: 'Kendall', offers_financing: true, offers_shuttle: false, rating: 3)
+        corolla = kendall.vehicles.create!(make: 'Toyota', model: 'Corolla', year: 2021, sale_pending: false)
+        camry = kendall.vehicles.create!(make: 'Toyota', model: 'Camry', year: 2022, sale_pending: false)
         lithia = Dealership.create!(name: 'Lithia', offers_financing: false, offers_shuttle: true, rating: 4)
         visit "/dealerships/#{kendall.id}" 
         
